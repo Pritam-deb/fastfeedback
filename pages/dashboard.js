274fcb99c51b88a39a72e7d3635fe7520673f553
Pritam <pritamdebnath297@gmail.com>
2021-11-22 03:30:54 +0530

    feat: fetched data from the API
    swr is an awesome tool!!!

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,11 +1,16 @@
+import useSWR from "swr";
+
 import { useAuth } from "@/lib/auth";
 import EmptyState from "@/components/EmptyState";
 import SiteTableSkeleton from "@/components/SiteTableSkeleton";
 import DashboardShell from "@/components/DashboardShell";
+import fetcher from "@/utils/fetcher";
 
 const Dashboard = () => {
+  const { data } = useSWR("/api/sites", fetcher);
+  console.log(data);
   const auth = useAuth();
-  if (!auth.user) {
+  if (!data) {
     return (
       <DashboardShell>
         <SiteTableSkeleton />
